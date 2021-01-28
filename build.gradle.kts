7d2b4e53e0ddd66f2306f0012a845d342efe6fee
Chloe Dawn <chloe@sapphic.dev>
2021-01-28 17:25:58 +0000

    Make server-safe, update workspace

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,60 +1,118 @@
+import net.minecraftforge.gradle.common.task.SignJar
+import org.gradle.util.GradleVersion
 import java.time.Instant
-import org.gradle.jvm.tasks.Jar
 
 plugins {
-  id("net.minecraftforge.gradle") version "3.0.184"
+  id("net.minecraftforge.gradle") version "4.0.9"
+  id("net.nemerosa.versioning") version "2.8.2"
   id("signing")
 }
 
 group = "dev.sapphic"
-version = "3.1.0"
+version = "1.0.0"
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_1_8
-  targetCompatibility = sourceCompatibility
+  withSourcesJar()
 }
 
 minecraft {
-  mappings("snapshot", "20200723-1.16.1")
+  mappings("snapshot", "20201028-1.16.3")
   runs {
-    with(create("client")) {
+    create("client") {
       workingDirectory = file("run").canonicalPath
       mods.create("iknowwhatimdoing").source(sourceSets["main"])
+      property("forge.logging.console.level", "debug")
+    }
+    create("server") {
+      workingDirectory = file("run").canonicalPath
+      mods.create("iknowwhatimdoing").source(sourceSets["main"])
+      property("forge.logging.console.level", "debug")
     }
   }
 }
 
 dependencies {
-  minecraft("net.minecraftforge:forge:1.16.2-33.0.22")
+  minecraft("net.minecraftforge:forge:1.16.5-36.0.7")
+  implementation("org.checkerframework:checker-qual:3.9.0")
 }
 
-tasks.withType<Jar> {
-  from("LICENSE")
-  archiveClassifier.set("forge")
-  manifest.attributes(mapOf(
-    "Specification-Title" to project.name,
-    "Specification-Vendor" to project.group,
-    "Specification-Version" to 1,
-    "Implementation-Title" to project.name,
-    "Implementation-Version" to project.version,
-    "Implementation-Vendor" to project.group,
-    "Implementation-Timestamp" to Instant.now().toString()
-  ))
-  finalizedBy("reobfJar")
-}
-
-tasks.withType<JavaCompile> {
-  with(options) {
-    isFork = true
-    isDeprecation = true
-    encoding = "UTF-8"
-    compilerArgs.addAll(listOf(
-      "-Xlint:all", "-parameters"
-    ))
+tasks {
+  compileJava {
+    with(options) {
+      release.set(8)
+      isFork = true
+      isDeprecation = true
+      encoding = "UTF-8"
+      compilerArgs.addAll(listOf("-Xlint:all", "-parameters"))
+    }
   }
-}
 
-signing {
-  useGpgCmd()
-  sign(configurations.archives.get())
+  jar {
+    from("/LICENSE")
+
+    manifest.attributes(
+      "Build-Timestamp" to Instant.now(),
+      "Build-Revision" to versioning.info.commit,
+      "Build-Jvm" to "${
+        System.getProperty("java.version")
+      } (${
+        System.getProperty("java.vendor")
+      } ${
+        System.getProperty("java.vm.version")
+      })",
+      "Built-By" to GradleVersion.current(),
+
+      "Implementation-Title" to project.name,
+      "Implementation-Version" to project.version,
+      "Implementation-Vendor" to project.group,
+
+      "Specification-Title" to "ForgeMod",
+      "Specification-Version" to "1.0.0",
+      "Specification-Vendor" to project.group
+    )
+
+    finalizedBy(reobf)
+  }
+
+  if (project.hasProperty("signing.mods.keyalias")) {
+    val keyalias = project.property("signing.mods.keyalias")
+    val keystore = project.property("signing.mods.keystore")
+    val password = project.property("signing.mods.password")
+
+    val signJar by creating(SignJar::class) {
+      dependsOn(reobf)
+
+      setAlias(keyalias)
+      setKeyStore(keystore)
+      setKeyPass(password)
+      setStorePass(password)
+      setInputFile(jar.get().archiveFile)
+      setOutputFile(inputFile)
+
+      doLast {
+        signing.sign(inputFile)
+      }
+    }
+
+    val signSourcesJar by creating(SignJar::class) {
+      val sourcesJar by getting(Jar::class) // No type-safe accessor
+
+      dependsOn(sourcesJar)
+
+      setAlias(keyalias)
+      setKeyStore(keystore)
+      setKeyPass(password)
+      setStorePass(password)
+      setInputFile(sourcesJar.archiveFile)
+      setOutputFile(inputFile)
+
+      doLast {
+        signing.sign(inputFile)
+      }
+    }
+
+    assemble {
+      dependsOn(signJar, signSourcesJar)
+    }
+  }
 }
--- src/main/java/dev/sapphic/iknowwhatimdoing/IKnowWhatImDoing.java
+++ src/main/java/dev/sapphic/iknowwhatimdoing/IKnowWhatImDoing.java
@@ -2,14 +2,28 @@ package dev.sapphic.iknowwhatimdoing;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.tutorial.TutorialSteps;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("iknowwhatimdoing")
 public final class IKnowWhatImDoing {
+  @SuppressWarnings({ "Convert2Lambda", "serial" })
   public IKnowWhatImDoing() {
-    FMLJavaModLoadingContext.get().getModEventBus().<FMLClientSetupEvent>addListener(e ->
-      Minecraft.getInstance().getTutorial().setStep(TutorialSteps.NONE));
+    ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> {
+      return Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (s, v) -> true);
+    });
+    DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> new DistExecutor.SafeRunnable() {
+      @Override
+      public void run() {
+        Minecraft.getInstance().execute(() -> {
+          Minecraft.getInstance().getTutorial().setStep(TutorialSteps.NONE);
+        });
+      }
+    });
   }
 }
