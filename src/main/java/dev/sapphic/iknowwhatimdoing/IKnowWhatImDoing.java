0dbd5015e58381342189f3b44f94e657b0121f65
Chloe Dawn <chloe@sapphic.dev>
2021-06-28 20:23:37 +0100

    Workaround Forge disallowing options.txt saving (#4)

--- src/main/java/dev/sapphic/iknowwhatimdoing/IKnowWhatImDoing.java
+++ src/main/java/dev/sapphic/iknowwhatimdoing/IKnowWhatImDoing.java
@@ -1,20 +1,23 @@
 package dev.sapphic.iknowwhatimdoing;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.MainMenuScreen;
 import net.minecraft.client.tutorial.TutorialSteps;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.GuiOpenEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.util.function.Consumer;
+
 @Mod("iknowwhatimdoing")
 public final class IKnowWhatImDoing {
-  @SuppressWarnings({ "Convert2Lambda", "serial" })
+  @SuppressWarnings("serial")
   public IKnowWhatImDoing() {
     ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> {
       return Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (s, v) -> true);
@@ -22,8 +25,14 @@ public final class IKnowWhatImDoing {
     DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> new DistExecutor.SafeRunnable() {
       @Override
       public void run() {
-        FMLJavaModLoadingContext.get().getModEventBus().<FMLLoadCompleteEvent>addListener(e -> {
-          e.enqueueWork(() -> Minecraft.getInstance().getTutorial().setStep(TutorialSteps.NONE));
+        MinecraftForge.EVENT_BUS.addListener(new Consumer<GuiOpenEvent>() {
+          @Override
+          public void accept(final GuiOpenEvent event) {
+            if (event.getGui() instanceof MainMenuScreen) {
+              Minecraft.getInstance().getTutorial().setStep(TutorialSteps.NONE);
+              MinecraftForge.EVENT_BUS.unregister(this);
+            }
+          }
         });
       }
     });
