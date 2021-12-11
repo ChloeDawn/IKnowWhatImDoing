415f76b2c1546910c424b3c2ab65643798bbd169
Chloe Dawn <chloe@sapphic.dev>
2021-12-11 14:51:35 +0000

    Add package-info.java and license headers

--- src/main/java/dev/sapphic/iknowwhatimdoing/package-info.java
+++ src/main/java/dev/sapphic/iknowwhatimdoing/package-info.java
@@ -14,17 +14,8 @@
  * limitations under the License.
  */
 
+@DefaultQualifier(NonNull.class)
 package dev.sapphic.iknowwhatimdoing;
 
-import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.tutorial.TutorialSteps;
-
-public final class IKnowWhatImDoing implements ClientModInitializer {
-  @Override
-  public void onInitializeClient() {
-    Minecraft.getInstance().execute(() -> {
-      Minecraft.getInstance().getTutorial().setStep(TutorialSteps.NONE);
-    });
-  }
-}
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.framework.qual.DefaultQualifier;
