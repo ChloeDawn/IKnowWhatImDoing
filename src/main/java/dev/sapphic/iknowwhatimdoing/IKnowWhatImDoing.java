fe1a874a2da02a97f2f9fb6db9843f9cde7eb642
Chloe Dawn <chloe@sapphic.dev>
2021-12-11 14:52:18 +0000

    Add toString implementation

--- src/main/java/dev/sapphic/iknowwhatimdoing/IKnowWhatImDoing.java
+++ src/main/java/dev/sapphic/iknowwhatimdoing/IKnowWhatImDoing.java
@@ -27,4 +27,9 @@ public final class IKnowWhatImDoing implements ClientModInitializer {
       Minecraft.getInstance().getTutorial().setStep(TutorialSteps.NONE);
     });
   }
+
+  @Override
+  public String toString() {
+    return "IKnowWhatImDoing";
+  }
 }
